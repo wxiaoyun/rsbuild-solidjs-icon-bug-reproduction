ac98c67c67afd905c36f37178261d5782a1e3a47
ForAeons <w.xy020203@gmail.com>
2024-06-29 12:01:50 +0800

    add reproduction

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BiSolidDownload } from "solid-icons/bi";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div class="content">
+      <BiSolidDownload size={100} />
+      <h1>Rsbuild with Solid</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+    </div>
+  );
+};
+
+export default App;
